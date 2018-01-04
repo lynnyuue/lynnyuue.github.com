b326bc7015fabf8661f88bddae3f7a563b75aee6
Lynn Yue <lyue@haiberg.com>
2018-01-04 18:33:21 +0800

    automatic commit

--- auto_push.cmd
+++ auto_push.cmd
@@ -1,6 +1,6 @@
 @echo off
 rem "git pull"
-git pull
+git pull origin
 rem "git check and commit"
 git status && git add * && git commit -a -m "automatic commit" && git push origin master
 rem "done"
