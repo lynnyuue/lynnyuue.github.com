75b838c906f188b05ddbeb9f5d8aad1e38556012
Lynn Yue <lyue@haiberg.com>
2018-01-16 09:49:26 +0800

    automatic commit

--- auto_push.cmd
+++ auto_push.cmd
@@ -1,4 +1,11 @@
 @echo off
+rem "check ssh key"
+IF EXIST /.ssh/id_rsa goto update
+rem "copy ssh kye"
+cp %USERPROFILE%"/.ssh/id_rsa" /.ssh/id_rsa
+cp %USERPROFILE%"/.ssh/id_rsa.pub" /.ssh/id_rsa.pub
+
+:update
 rem "git pull"
 git pull origin
 rem "git check and commit"
